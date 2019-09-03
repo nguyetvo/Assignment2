0b1e0cdf69baab3a0c335856c75cebb9100ce066
nguyetvo <30364033+nguyetvo@users.noreply.github.com>
2019-09-03 23:11:56 +0700

    Add files via upload

--- UpdateData.php
+++ UpdateData.php
@@ -79,7 +79,7 @@
 
         $db = parse_url(getenv("DATABASE_URL"));
         $pdo = new PDO("pgsql:" . sprintf(
-            "host=ec2-54-83-9-36.compute-1.amazonaws.com;port=5432;user=mnvkrpgighmovm;password=ec88450374a0be701bd72da2753e03f48af3f3e48c9644b862a58dd677d22cd5;dbname=dfv6jafh0t2m8e",
+            "host=ec2-54-225-72-238.compute-1.amazonaws.com;port=5432;user=rxtdhfqtpxrhhs;password=6f6584dbda6217790c95fefe913f8c1697b8d1467b37b8c44a1ccfb62b66d7cf;dbname=d5dfoim75pqir4",
             $db["host"],
             $db["port"],
             $db["user"],
